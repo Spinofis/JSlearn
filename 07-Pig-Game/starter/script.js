717f924ac2ceb690697910c16e3125285129c917
bartp <bartek2560@gmail.com>
2022-04-01 19:11:24 +0200

    pig game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -6,18 +6,36 @@ let currentScore = 0;
 let player1Score = 0;
 let player2Score = 0;
 
+const minDice = 0;
+const maxDice = 6;
+const score1 = document.getElementById('score--0');
+const score2 = document.getElementById('score--1');
+const dice = document.querySelector('.dice');
+
+newGame();
 
 function rollDice() {
     console.log("rollDice");
     console.log(player);
+
+    let diceNumber = getDiceNumber();
+    console.log('dice ' + dice);
+    dice.src = `dice-${dice}.png`;
+    dice.classList.remove('hidden');
 }
 
-function hold(){
+function hold() {
     console.log('hold')
 }
 
-function newGame(){
+function newGame() {
+    score1.textContent = 0;
+    score2.textContent = 0;
+    dice.classList.add('hidden');
+}
 
+function getDiceNumber() {
+    return Math.floor(Math.random() * (maxDice - minDice + 1)) + minDice;
 }
 
 document.querySelector(".btn--roll")
